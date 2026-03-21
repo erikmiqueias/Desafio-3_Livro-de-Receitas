e35ed4a72685d9786adefa827d878ae5d8464f6d
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2026-03-20 21:59:14 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-center text-4xl font-semibold text-stone-900">
+        Desafio 3: Livro de Receitas
+      </h1>
+    </main>
+  )
+}
+
+export default App
