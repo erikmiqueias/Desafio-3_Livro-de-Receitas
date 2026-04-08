e0fbf6ef164767b9daf1ce4ed5f3fce5ecab9aaf
erikmiqueias <erikmiqueiaspereira@outlook.com>
2026-04-08 15:12:52 -0400

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
+import { MenuLayout } from "./layouts/MenuLayout";
+
 export const App = () => {
-  return (
-    <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-center text-4xl font-semibold text-stone-900">
-        Desafio 3: Livro de Receitas
-      </h1>
-    </main>
-  );
+  return <MenuLayout />;
 };
---
+++ src/layouts/MenuLayout.tsx
@@ -0,0 +1,3 @@
+export const MenuLayout = () => {
+  return <div>MenuLayout</div>;
+};
