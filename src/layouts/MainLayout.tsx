c5edc0c558bb1d70dcd8ecd4ce9ff07b2a07e9f9
erikmiqueias <erikmiqueiaspereira@outlook.com>
2026-04-08 21:24:22 -0400

    fix: dvh in MainLayout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,7 +4,7 @@ import { Card } from "@/components/ui/card";
 
 export const MainLayout = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-slate-50 p-6">
+    <div className="flex min-h-dvh items-center justify-center bg-slate-50 p-6">
       <Card className="w-full max-w-360 overflow-hidden p-3 shadow-xl">
         <Outlet />
       </Card>
